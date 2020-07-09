75149d3d1ce798cee59d1ba201839f6ce49e0218
pjheric <pjheric1219@gmail.com>
2020-07-09 15:35:06 +0900

    Stuck here

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -5,8 +5,46 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
-    
-   public void Fire()
+    AttackerSpawner myLaneSpawner; 
+    private void Start()
+    {
+        SetLaneSpawner(); 
+    }
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            //shoot- change anim state to shooting  
+        }
+        else
+        {
+            //wait- change anim state to idle 
+        }
+    }
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>(); 
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon); 
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner; 
+            }
+        }
+    }
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false; 
+        }
+        else
+        {
+            return true; 
+        }
+    }
+    public void Fire()
     {
         Instantiate(projectile, gun.transform.position, transform.rotation); 
     }
